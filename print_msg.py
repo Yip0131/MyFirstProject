125cea941f529d1d025a98a5d224dc8975fab38b
Yip0131 <yeewenyip@gmail.com>
2026-04-09 16:16:57 +0800

    Merged Commit

---
+++ print_msg.py
@@ -0,0 +1,10 @@
+# Simple print statement
+print("Hello, World!")
+
+# Or with a variable
+msg = "Your message here"
+print(msg)
+
+# Or taking user input
+msg = input("Enter your message= ")
+print(msg)
